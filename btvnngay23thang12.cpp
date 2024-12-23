3ebdd68043ed675752bb2d80ae25640d8f6fbb3c
thanhthanh2005 <buithanh29092005@gmail.com>
2024-12-23 23:39:26 +0700

    23/12

--- btvnngay23thang12.cpp
+++ btvnngay23thang12.cpp
@@ -1,112 +1,122 @@
-#include <iostream> 
-using namespace std; 
+#include <iostream>
+using namespace std;
 
-struct Node { 
-    int data; 
-    Node *left; 
-    Node *right; 
-    int height; 
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    int height;
 };
 
 Node* newnode(int data) {
-	Node* p = new Node;
-	p->data = data;
-	p->height = 1;
-	p->left = p->right = nullptr;
-	return p;
-} 
-
-
-int height(Node *T) { 
-    if (T == nullptr) return -1; 
-    return T->height; 
-} 
-
-void updateHeight(Node* t) {
-	t->height = 1 + max(height(t->left), height(t->right)); 
+    Node* p = new Node;
+    p->data = data;
+    p->height = 1;
+    p->left = p->right = nullptr;
+    return p;
 }
 
-Node* rightRotate(Node *t) { 
-    Node *tmp1 = t->left; 
-    Node *tmp2 = tmp1->right; 
-    tmp1->right = t; 
-    t->left = tmp2;  
+int height(Node* T) {
+    if (T == nullptr) return -1;
+    return T->height;
+}
+
+void updateHeight(Node* t) {
+    t->height = 1 + max(height(t->left), height(t->right));
+}
+
+void rotateRight(Node*& t) {
+    Node* tmp1 = t->left;
+    Node* tmp2 = tmp1->right;
+    tmp1->right = t;
+    t->left = tmp2;
     updateHeight(t);
     updateHeight(tmp1);
-    return tmp1; 
-} 
+    t = tmp1;
+}
 
-
-Node *leftRotate(Node *t) { 
-    Node *tmp1 = t->right; 
-    Node *tmp2 = tmp1->left; 
-    tmp1->left = t; 
-    t->right = tmp2;  
+void rotateLeft(Node*& t) {
+    Node* tmp1 = t->right;
+    Node* tmp2 = tmp1->left;
+    tmp1->left = t;
+    t->right = tmp2;
     updateHeight(t);
     updateHeight(tmp1);
-    return tmp1; 
-} 
+    t = tmp1;
+}
 
-int getBalance(Node* T) { 
-    if (T == nullptr) return 0; 
-    else return height(T->left) - height(T->right); 
-} 
+void doubleRotateRight(Node*& t) {
+    rotateLeft(t->left);
+    rotateRight(t);
+}
 
+void doubleRotateLeft(Node*& t) {
+    rotateRight(t->right);
+    rotateLeft(t);
+}
 
-Node* insert(Node* node, int data) { 
+int updateBalance(Node* T) {
+    if (T == nullptr) return 0;
+    else return height(T->left) - height(T->right);
+}
 
-    if (node == nullptr) return newnode(data); 
+void balance(Node*& node) {
+    if (!node) return;
 
-    if (data <= node->data) node->left = insert(node->left, data); 
-    else if (data > node->data) node->right = insert(node->right, data); 
-    else return node; 
-    
     updateHeight(node);
+    int b = updateBalance(node);
 
-    int balance = getBalance(node); 
+    if (b > 1) { 
+        if (updateBalance(node->left) >= 0)
+            rotateRight(node); 
+        else
+            doubleRotateRight(node); 
+    } else if (b < -1) { 
+        if (updateBalance(node->right) <= 0)
+            rotateLeft(node); 
+        else
+            doubleRotateLeft(node); 
+    }
+}
 
-    if (balance > 1 && data < node->left->data) 
-        return rightRotate(node); 
+void insert(Node*& node, int data) {
+    if (node == nullptr) {
+        node = newnode(data);
+        return;
+    }
 
-    if (balance < -1 && data > node->right->data) 
-        return leftRotate(node); 
+    if (data <= node->data)
+        insert(node->left, data);
+    else if (data > node->data)
+        insert(node->right, data);
 
-    if (balance > 1 && data > node->left->data) { 
-        node->left = leftRotate(node->left); 
-        return rightRotate(node); 
-    } 
-    if (balance < -1 && data < node->right->data) { 
-        node->right = rightRotate(node->right); 
-        return leftRotate(node); 
-    } 
-    return node; 
-} 
- 
-void print(Node *t) { 
-    if (t != nullptr) { 
-        cout << t->data << " "; 
-        print(t->left); 
-        print(t->right); 
-    } 
-} 
+    balance(node); 
+}
 
-int main() { 
-    Node *root = nullptr; 
+void print(Node* t) {
+    if (t != nullptr) {
+        cout << t->data << " ";
+        print(t->left);
+        print(t->right);
+    }
+}
+
+int main() {
+    Node* root = nullptr;
     
-    root = insert(root, 17); 
-    root = insert(root, 23); 
-    root = insert(root, 201); 
-    root = insert(root, 98); 
-    root = insert(root, 67); 
-    root = insert(root, 83);
-	root = insert(root, 13); 
-	root = insert(root, 23); 
-	root = insert(root, 10); 
-	root = insert(root, 191); 
-	root = insert(root, 84); 
-	root = insert(root, 58);  
-    
-    print(root); 
-    
-    return 0; 
+    insert(root, 17);
+    insert(root, 23);
+    insert(root, 201);
+    insert(root, 98);
+    insert(root, 67);
+    insert(root, 83);
+    insert(root, 13);
+    insert(root, 23);
+    insert(root, 10);
+    insert(root, 191);
+    insert(root, 84);
+    insert(root, 58);
+
+    print(root);
+
 }
---
+++ nhap.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    
+};
+
+
+int main() {
+
+}
