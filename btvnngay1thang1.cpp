438bcc865c097a8d0fcd5c5f5238d13f4f7f2978
thanhthanh2005 <buithanh29092005@gmail.com>
2025-01-01 12:04:23 +0700

    up

--- btvnngay1thang1.cpp
+++ btvnngay1thang1.cpp
@@ -19,22 +19,35 @@ HP-HD : D13
 */
 
 const int n = 11;
+void add_path(int data[n][n], string pr1, string pr2, int des) {
+    string province[11] = { "HN", "ST", "TN", "BN", "BG", "UB", "HP",
+                            "HD", "HY", "PL", "HB" };
+    int m1, m2;
+    for (int j = 0; j < 11; j++) {
+        if (pr1 == province[j]) m1 = j;
+        if (pr2 == province[j]) m2 = j;
+    }
+    data[m1][m2] = data[m2][m1] = des;
+}
 
 int main() {
     int data[n][n] = { 0 };
-    string province[11] = { "HN", "ST", "TN", "BN", "BG", "UB", "HP",
-                            "HD", "HY", "PL", "HB" };
-    for (int i = 1; i <= 13; i++) {
-        
-        string pr1, pr2;
-        cin >> pr1 >> pr2;
-        int m1, m2;
-        for (int j = 0; j < 11; j++) {
-            if (pr1 == province[j]) m1 = j;
-            if (pr2 == province[j]) m2 = j;
-        }
-        data[m1][m2] = data[m2][m1] = i;
-    }
+    add_path(data, "HN", "HD", 1);
+    add_path(data, "HD", "HY", 2);
+    add_path(data, "HY", "PL", 3);
+    add_path(data, "HN", "PL", 4);
+    add_path(data, "HN", "HB", 5);
+    add_path(data, "HN", "ST", 6);
+    add_path(data, "HN", "TN", 7);
+    add_path(data, "HN", "BN", 8);
+    add_path(data, "BN", "BG", 9);
+    add_path(data, "BG", "UB", 10);
+    add_path(data, "BN", "UB", 11);
+    add_path(data, "UB", "HP", 12);
+    add_path(data, "HP", "HD", 13);
+    
+
+
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             cout << data[i][j] << ' ';
